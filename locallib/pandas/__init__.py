30abe7698af8af6caa91bf95f377856a36c23f37
picarro-dsoler <dsoler@picarro.com>
2026-06-01 09:52:06 +0000

    Added the first version of the pandas accesor of the number of passes

--- locallib/pandas/__init__.py
+++ locallib/pandas/__init__.py
@@ -5,12 +5,14 @@ Pandas utilities and extensions for locallib
 from .Timezone import TimezoneAccessor, COMMON_TIMEZONE_MAPPINGS
 from .DBQuery import DBAccessor as PandasDBAccessor
 from .EmissionRates import EmissionRateAccessor, SCFH_TO_G_PER_HOUR, SCFH_TO_SLPM_FACTOR
-
+from .NOP import NOPAccessor
 __all__ = [
     'TimezoneAccessor', 
     'COMMON_TIMEZONE_MAPPINGS',
     'SCFH_TO_G_PER_HOUR',
     'SCFH_TO_SLPM_FACTOR',
     'PandasDBAccessor',
-    'EmissionRateAccessor'
+    'EmissionRateAccessor',
+    'NOPAccessor'
+
 ]
