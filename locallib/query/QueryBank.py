d5c35be1d83d6b83e6e01e75ac5f067249d3d1c1
picarro-dsoler <dsoler@picarro.com>
2026-06-17 23:04:19 +0000

    added changes onto the querybank:

--- locallib/query/QueryBank.py
+++ locallib/query/QueryBank.py
@@ -98,6 +98,56 @@ def get_reports(customer_name, table_name = None, years=None, final_checkbox = T
     """
     return query
 
+@setup_query
+def get_report_info(report_table, table_name = None):
+    report_id_filter = f"R.Id IN (SELECT ReportId FROM {report_table})"
+    if table_name is not None:
+        into_clause = f"INTO {table_name}"
+    else:
+        into_clause = ""
+
+    query = f"""
+    SELECT 
+        C.Name AS CustomerName,
+        CASE
+            WHEN ReportType.Description = 'Compliance' THEN CONCAT('CR-', SUBSTRING(CONVERT(nvarchar(50), R.Id), 1, 6))
+            WHEN ReportType.Description = 'Emissions' THEN CONCAT('ER-', SUBSTRING(CONVERT(nvarchar(50), R.Id), 1, 6))
+            ELSE CONCAT('CR-', SUBSTRING(CONVERT(nvarchar(50), R.Id), 1, 6))
+        END AS ReportName,
+        R.Id AS ReportId,
+        R.ReportTitle AS ReportTitle,
+        L.Title AS Label,
+        R.DateStarted AS ReportDate,
+        RA.ExternalId AS BoundaryName,
+        RAC.AssetLengthKM AS ReportAssetLengthKm,
+        RC.PercentCoverageAssets AS ReportPercentCoverageAssets,
+        RAC.AssetLengthKM * RC.PercentCoverageAssets AS AssetCoveredLengthKm,
+        RAC.DistributionPipeCoveredKm,
+        RAC.DistributionPipeKm,
+        RAC.DistributionPipePercentCovered,
+        RAC.ServicePipeKm,
+        RAC.ServicePipeCoveredKm
+    {into_clause}
+    FROM
+        Report R
+    LEFT JOIN Customer C ON
+        R.CustomerId = C.Id
+    LEFT JOIN ReportLabel RL ON
+        R.Id = RL.ReportId
+    LEFT JOIN Label L ON
+        RL.LabelId = L.Id
+    LEFT JOIN ReportType ON
+        R.ReportTypeId = ReportType.Id
+    LEFT JOIN ReportArea RA ON R.Id = RA.ReportId
+    LEFT JOIN ReportCompliance RC ON R.Id = RC.ReportId
+    LEFT JOIN ReportAreaCovered RAC ON R.Id = RAC.ReportId
+    WHERE
+        {report_id_filter}
+    AND L.Title = 'Final Checkbox'
+        AND RL.IsActive = 1 
+    """
+    return query
+
 def get_final_reports(customer_name, table_name = None, years=None):
     if isinstance(customer_name, list):
         customer_name_clauses = []
